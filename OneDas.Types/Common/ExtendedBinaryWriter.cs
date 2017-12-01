ed58600f0e99c0f0ae4d8a5949a8146bd00d5da9
Vincent Wilms <vwilms@outlook.com>
2017-12-01 17:13:17 +0100

    add ExtendedBinaryWriter

---
+++ OneDas.Types/Common/ExtendedBinaryWriter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+namespace OneDas.Common
+{
+    public class ExtendedBinaryWriter : BinaryWriter
+    {
+        public ExtendedBinaryWriter(Stream stream) : base(stream)
+        {
+            //
+        }
+
+        private void WriteBE(byte[] data)
+        {
+            Array.Reverse(data);
+            base.Write(data);
+        }
+
+        public void WriteBE(short value)
+        {
+            this.WriteBE(BitConverter.GetBytes(value));
+        }
+
+        public void WriteBE(ushort value)
+        {
+            this.WriteBE(BitConverter.GetBytes(value));
+        }
+
+        public void WriteBE(long value)
+        {
+            this.WriteBE(BitConverter.GetBytes(value));
+        }
+
+        public void WriteBE(ulong value)
+        {
+            this.WriteBE(BitConverter.GetBytes(value));
+        }
+
+        public void WriteBE(float value)
+        {
+            this.WriteBE(BitConverter.GetBytes(value));
+        }
+
+        public void WriteBE(double value)
+        {
+            this.WriteBE(BitConverter.GetBytes(value));
+        }
+    }
+}
