cdecdb88eb5881e8ecead6f5f3a979eac23e010a
Vincent Wilms <vwilms@outlook.com>
2018-04-12 10:16:00 +0200

    remove copy action

--- build/PostBuild.bat
+++
@@ -1,6 +0,0 @@
-set "sourceDir=%~1."
-set "localTargetDir=%localappdata%\OneDAS\Core\plugin\%pluginName%"
-
-robocopy "%sourceDir%" "%localTargetDir%" /MIR /xf OneDas.Types.dll /R:0
-
-if errorlevel 1 exit 0 else exit %errorlevel%
