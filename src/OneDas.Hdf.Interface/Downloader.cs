19fa2e38eab2f42124a989fb7f899afef24cb4cc
Vincent Wilms <vwilms@outlook.com>
2019-05-17 13:51:12 +0200

    Update OneDas.Hdf.Interface.

--- src/OneDas.Hdf.Interface/Downloader.cs
+++ src/OneDas.Hdf.Interface/Downloader.cs
@@ -1,51 +1,60 @@
 ﻿using Microsoft.AspNetCore.SignalR.Client;
-using OneDas.Infrastructure;
 using System;
-using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
+using System.Net;
 using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace OneDas.Hdf.Interface
 {
-    public class Logger
-    {
-        Action<string> _logAction;
-
-        public Logger(Action<string> logAction)
-        {
-            _logAction = logAction;
-        }
-
-        public void Log(string message)
-        {
-            _logAction?.Invoke(message);
-        }
-    }
-
-
     public class Downloader
     {
-        HubConnection _connection;
+        string _baseAddress;
+        string _url;
         Logger _logger;
+        HubConnection _connection;
+        ManualResetEventSlim _manualResetEvent;
+        CancellationTokenSource _cts;
 
         static Downloader()
         {
-            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+            AppDomain.CurrentDomain.AssemblyResolve += Downloader.CurrentDomain_AssemblyResolve;
         }
 
         private static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
         {
-            return Assembly.Load(args.Name.Split(',').First());
+            string assemblyName;
+
+            assemblyName = args.Name.Split(',').First();
+
+            if (!args.Name.Contains(".resources"))
+            {
+                return Assembly.Load(assemblyName);
+            }
+
+            return null;
         }
 
         public Downloader(string hostName, int port, Action<string> logAction)
         {
+            _baseAddress = $"http://{hostName}:{port}/";
+            _manualResetEvent = new ManualResetEventSlim();
+            _cts = new CancellationTokenSource();
+
             _connection = this.BuildHubConnection(hostName, port, "broadcaster");
 
             _connection.On("SendProgress", (double percent, string message) =>
             {
                 _logger?.Log($"{percent,3:0}%: {message}\n");
+            });
+
+            _connection.On("SendUrl", (string url) =>
+            {
+                _url = url;
+                _cts.Cancel();
             });
 
             _logger = new Logger(logAction);
@@ -63,16 +72,75 @@ namespace OneDas.Hdf.Interface
             }
         }
 
-        public async Task<string> Download(DateTime dateTimeBegin, DateTime dateTimeEnd, FileGranularity fileGranularity, string sampleRateDescription, string campaignPath, List<string> variableNameSet)
+        public Task DownloadAndExtract(DownloadSettings settings, string targetDirectoryPath)
         {
-            try
+            return Task.Run(() =>
             {
-                return await _connection.InvokeAsync<string>("Download", dateTimeBegin, dateTimeEnd, "MAT73", fileGranularity, sampleRateDescription, campaignPath, variableNameSet);
-            }
-            catch (AggregateException ex)
-            {
-                throw ex.InnerException;
-            }
+                string downloadFilePath;
+                string entryFilePath;
+
+                WebClient webClient;
+
+                // get URL
+                _connection.InvokeAsync<string>(
+                        "Download",
+                        settings.DateTimeBegin,
+                        settings.DateTimeEnd,
+                        "MAT73",
+                        settings.FileGranularity,
+                        settings.SampleRateDescription,
+                        settings.CampaignPath,
+                        settings.VariableNameSet);
+
+                try
+                {
+                    _manualResetEvent.Wait(_cts.Token);
+                }
+                catch (OperationCanceledException)
+                {
+                    //
+                }
+
+                if (string.IsNullOrWhiteSpace(_url))
+                {
+                    throw new Exception("No URL received from the server.");
+                }
+
+                downloadFilePath = Path.GetTempFileName();
+
+                try
+                {
+                    // download data
+                    _logger.Log($"Downloading ZIP file from {_baseAddress}{_url} to {downloadFilePath}.\n");
+
+                    webClient = new WebClient();
+                    webClient.DownloadFile($"{_baseAddress}{_url}", downloadFilePath);
+
+                    // unzip data
+                    using (var archive = ZipFile.OpenRead(downloadFilePath))
+                    {
+                        archive.Entries.ToList().ForEach(entry =>
+                        {
+                            entryFilePath = Path.Combine(targetDirectoryPath, entry.FullName);
+
+                            _logger.Log($"Unzipping file {entry.FullName}.\n");
+
+                            if (!File.Exists(entryFilePath))
+                            {
+                                Directory.CreateDirectory(Path.GetDirectoryName(entryFilePath));
+                                ZipFileExtensions.ExtractToFile(entry, entryFilePath);
+                            }
+                        });
+                    }
+                }
+                finally
+                {
+                    if (File.Exists(downloadFilePath))
+                    {
+                        File.Delete(downloadFilePath);
+                    }
+                }
+            });
         }
 
         public void Disconnect()
