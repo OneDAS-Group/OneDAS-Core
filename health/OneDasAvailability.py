bd30e0b1b7156e819a0ebc05c6243707edc11b44
Apollo3zehn <vwilms@outlook.com>
2020-05-03 17:42:58 +0200

    Add health checker.

---
+++ health/OneDasAvailability.py
@@ -0,0 +1,53 @@
+import statistics
+from datetime import datetime, timezone
+from typing import Dict
+
+from signalrcore_async.hub_connection_builder import HubConnectionBuilder
+
+from BaseTypes import Checker, CheckResult
+
+
+class OneDasAvailabilityCheck(Checker):
+    Id: str = "onedas-availability"
+    Address: str
+    Port: int
+    CampaignName: str
+    LimitDays: int
+
+    def __init__(self, settings: Dict[str, str]):
+        self.Address = settings["address"]
+        self.Port = int(settings["port"])
+        self.CampaignName = settings["campaign"]
+        self.LimitDays = int(settings["limit_days"])
+
+    async def DoCheckAsync(self) -> CheckResult:
+
+        protocol = "ws"
+        host = self.Address
+        port = self.Port
+        hub = "datahub"
+        hub_url = f"{protocol}://{host}:{port}/{hub}"
+
+        connection = HubConnectionBuilder()\
+            .with_url(hub_url)\
+            .build()
+
+        name = f"{self.CampaignName}"
+
+        try:
+            await connection.start()
+
+            campaignName = self.CampaignName
+            begin = datetime(2020, 3, 1, 0, 0, 0, tzinfo=timezone.utc)
+            end = datetime(2020, 4, 1, 0, 0, 0, tzinfo=timezone.utc)
+            availability = await connection.invoke("GetDataAvailabilityStatistics", [campaignName, begin, end])
+            value = statistics.mean(availability["data"])
+
+            return CheckResult(name, False, f"Availability: {value:.0f} %")
+
+        except Exception as ex:
+            print(ex)
+            return CheckResult(name, True, "Could not communicate to OneDAS.")
+
+        finally:
+            await connection.stop()
