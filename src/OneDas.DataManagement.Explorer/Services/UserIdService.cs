4e05599a67b6d6c0d3dc29b324fd8080655acf5e
Vincent Wilms <vwilms@outlook.com>
2021-01-08 14:48:34 +0100

    Fix null http context.

--- src/OneDas.DataManagement.Explorer/Services/UserIdService.cs
+++ src/OneDas.DataManagement.Explorer/Services/UserIdService.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Authorization;
+﻿using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Http;
+using System.Net;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -10,7 +10,6 @@ namespace OneDas.DataManagement.Explorer.Services
     {
         #region Fields
 
-        private IHttpContextAccessor _httpContextAccessor;
         private AuthenticationStateProvider _authenticationStateProvider;
 
         #endregion
@@ -19,7 +18,12 @@ namespace OneDas.DataManagement.Explorer.Services
 
         public UserIdService(IHttpContextAccessor httpContextAccessor, AuthenticationStateProvider authenticationStateProvider)
         {
-            _httpContextAccessor = httpContextAccessor;
+            // https://github.com/dotnet/aspnetcore/issues/17585
+            // https://github.com/dotnet/aspnetcore/issues/18183
+            // https://github.com/dotnet/aspnetcore/issues/14090
+            this.User = httpContextAccessor.HttpContext?.User;
+            this.RemoteIpAddress = httpContextAccessor.HttpContext?.Connection.RemoteIpAddress;
+
             _authenticationStateProvider = authenticationStateProvider;
         }
 
@@ -27,12 +31,15 @@ namespace OneDas.DataManagement.Explorer.Services
 
         #region Properties
 
-        public ClaimsPrincipal User => _httpContextAccessor.HttpContext.User;
+        public ClaimsPrincipal User { get; }
+
+        public IPAddress RemoteIpAddress { get; }
 
         #endregion
 
         #region Methods
 
+        // Use this method only in conjunction with blazor invoked actions
         public async Task<ClaimsPrincipal> GetUserAsync()
         {
             var state = await _authenticationStateProvider.GetAuthenticationStateAsync();
@@ -48,8 +55,7 @@ namespace OneDas.DataManagement.Explorer.Services
             else
                 username = "anonymous";
 
-            var remoteIpAddress = _httpContextAccessor.HttpContext.Connection.RemoteIpAddress;
-            return $"{username} ({remoteIpAddress})";
+            return $"{username} ({this.RemoteIpAddress})";
         }
 
         #endregion
